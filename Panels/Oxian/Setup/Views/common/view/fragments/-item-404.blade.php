d3feab2f2a086a811f79d337d1e7d3a7a181a793
Paris Bala <arianbala56@gmail.com>
2025-10-17 20:40:04 +0600

    added

---
+++ Panels/Oxian/Setup/Views/common/view/fragments/-item-404.blade.php
@@ -0,0 +1,19 @@
+<div class="d-flex flex-row justify-content-center align-items-center" style="height: 400px;">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="text-center">
+                <div>
+                    <div class="row justify-content-center">
+                        <div class="col-sm-4">
+                            <div class="error-img">
+                                <img src="{{url('images/system/404-error.png')}}" alt="" class="img-fluid mx-auto d-block"  style="opacity: .5">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h4 class="text-uppercase mt-4">Sorry, page not found -   404  </h4>
+                <p class="text-muted">Your requested resource wan not found, either deleted or remved ny the user </p>
+            </div>
+        </div>
+    </div>
+</div>
