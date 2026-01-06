7112ed5162a52d9ee6bcaf68c02416457e220ef8
Paris Ariyan <Paris Ariyan>
2026-01-06 09:22:04 +0600

    added

--- InstallPx/V.12/resources/js/TestComponent.jsx
+++ InstallPx/V.12/resources/js/TestComponent.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { createRoot } from 'react-dom/client';
 export default function TestComponent() {
   return (
     <div>TestComponent</div>
   )
+}
+if (document.getElementById('TestComponent')) {
+    createRoot(document.getElementById('TestComponent')).render(<TestComponent />)
 }
